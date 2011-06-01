c88c21ca8e8b8b2483d6badfc65a2d0642fdad37
Drew Wagner <drewm1980@gmail.com>
2011-06-01 18:29:15 -0500

    Alignment stage benchmark

--- figures/alignment_runtime_graph.m
+++ figures/alignment_runtime_graph.m
@@ -14,16 +14,18 @@ gtx580 = gtx580(:,2);
 
 close all;
 h = figure(7);
-plot( userCount, gtx480, 'kx-' , userCount, gtx580, 'ko-' , userCount, cpu1, 'ks-' , userCount, cpu2, 'kd-');
-legend('gtx480', 'gtx580', '2 x E5530, single instance', '2 x E5530, 1 instance per core');
+plot( userCount, gtx480, 'k-.', userCount, gtx580, 'k-' , userCount, cpu1, 'k:' , userCount, cpu2, 'k--');
+legend('gtx480 GPU', 'gtx580 GPU', '2 x E5530 CPU, single instance', '2 x E5530 CPU, 1 instance per core', 'Location','NorthWest');
 xlabel('Number of training users');
 ylabel('Elapsed time (s)');
 
 set (h,'windowstyle','normal');               
 set (h,'Units','Inches');                    
-%windowSize = [0 0 3.75 2];
-windowSize = [0 0 20 15];
+windowSize = [0 0 3.75 2];
+windowSize = windowSize*4;
+%windowSize = [0 0 20 15];
 set(h,'position', windowSize);
 set(h, 'PaperPosition', windowSize);
 print(h, 'alignment_runtime_graph.eps', '-deps2');
+print(h, 'alignment_runtime_graph.png', '-dpng');
 
