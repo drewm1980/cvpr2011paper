c8e6f8fbdfccd4a015c324da0dadf9f908256364
Drew Wagner <drewm1980@gmail.com>
2011-06-04 21:05:48 -0500

    New figure 4, no markers.

--- figures/alignment_runtime_graph.m
+++ figures/alignment_runtime_graph.m
@@ -19,12 +19,12 @@ gtx580 = gtx580(1:2:end,2);
 close all;
 h = figure(7);
 
-%plot( userCount, gtx480, 'k-.', userCount, gtx580, 'k-' , userCount, cpu1, 'k:' , userCount, cpu2, 'k--');
-%legend('gtx480 GPU', 'gtx580 GPU', '2 x E5530 CPU, single instance', '2 x E5530 CPU, 1 instance per core', 'Location','NorthWest');
+plot( userCount, gtx480, 'k-', userCount, cpu1, 'k:' , userCount, cpu2, 'k--');
+legend('GPU', 'CPU, library threading', 'CPU, manual threading', 'Location','NorthWest');
 
-%plot( userCount, gtx580, 'k-' , userCount, cpu1, 'k:' , userCount, cpu2, 'k--');
-plot( userCount, gtx580, 'kx-' , userCount, cpu1, 'k*-' , userCount, cpu2, 'ko-');
-legend('gtx580 GPU', '2 x E5530 CPU, single instance', '2 x E5530 CPU, 1 instance per core','Location', 'NorthWest');
+%plot( userCount, GTX 580, 'k-' , userCount, cpu1, 'k:' , userCount, cpu2, 'k--');
+%plot( userCount, GTX 580, 'kx-' , userCount, cpu1, 'k*-' , userCount, cpu2, 'ko-');
+%legend('gtx580 GPU', '2 x E5530 CPU, single instance', '2 x E5530 CPU, 1 instance per core','Location', 'NorthWest');
 
 xlabel('Number of training users');
 ylabel('Elapsed time (s)');
@@ -33,8 +33,8 @@ set (h,'windowstyle','normal');
 %set (h,'Units','Inches'); % matlab
 set (h,'Units','inches'); % octave
 windowSize = [0 0 3.75 2];
-windowSize = windowSize*0.8;
-%windowSize = windowSize*1.5;
+%windowSize = windowSize*0.8;
+windowSize = windowSize*1.2;
 %windowSize = windowSize*4;
 %windowSize = [0 0 20 15];
 set(h,'position', windowSize);
